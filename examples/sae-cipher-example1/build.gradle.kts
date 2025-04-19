88f23e976cde5329c29ca05a0364e903fee937bc
parksay <hellobigworld.parksay@gmail.com>
2025-04-19 15:41:25 +0900

    examples ver

--- examples/sae-cipher-example1/build.gradle.kts
+++ examples/sae-cipher-example1/build.gradle.kts
@@ -11,7 +11,7 @@ repositories {
 }
 
 dependencies {
-    implementation ("com.github.parksay:202504-saecipher:0.1.0")
+    implementation ("com.github.parksay:202504-saecipher:0.1.1")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
